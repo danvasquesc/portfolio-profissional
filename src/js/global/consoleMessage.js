6d8802bc8cf1082c9a43f04a3354a4583bf0c8da
Dan Vasques <dan.vasques@outlook.com.br>
2025-10-28 18:38:21 -0300

    feat: Adicionado funções JS globais

---
+++ src/js/global/consoleMessage.js
@@ -0,0 +1,14 @@
+export function initConsoleMessage() {
+  console.log(
+    "%cDan Vasques",
+    "color: #FF4500; font-size: 24px; font-weight: bold;"
+  );
+  console.log(
+    "%cSite desenvolvido com HTML, CSS e JavaScript puro",
+    "color: #b0b0b0; font-size: 12px;"
+  );
+  console.log(
+    "%cLinkedIn → https://linkedin.com/in/dan-vasques-carvalho",
+    "color: #FF4500;"
+  );
+}
--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,5 +1,9 @@
 import { initNavbar } from "../../js/pages/nav.js";
+import { initSmoothScroll } from "../../js/global/smoothScroll.js";
+import { initConsoleMessage } from "../../js/global/consoleMessage.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initNavbar();
+  initSmoothScroll();
+  initConsoleMessage();
 });
